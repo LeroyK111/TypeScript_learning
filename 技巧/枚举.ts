b437f3dc272daa99f10485f1886e0eafd0871c13
LeroyK <bridieiybnf@gmail.com>
2023-05-06 19:53:58 +0800

    1

--- 技巧/枚举.ts
+++ 技巧/枚举.ts
@@ -9,3 +9,17 @@ type method = keyof typeof HttpMethod;
 //   Get: 0,
 //   Post: 1,
 // };
+
+
+
+enum E {
+  A = 10 * 10, // Numeric literal enum member
+  B = 'foo', // String literal enum member
+  C = Math.random(), // Opaque computed enum member
+}
+
+function getStringValue(e: E): string {
+  return String(e);
+}
+
+const val = getStringValue(E.A); // "100"
