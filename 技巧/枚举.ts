23bd2501e1775305dd996ec593df3fc14d03186d
LeroyK <bridieiybnf@gmail.com>
2023-05-31 19:50:35 +0800

    1

--- 技巧/枚举.ts
+++ 技巧/枚举.ts
@@ -24,6 +24,7 @@ function getStringValue(e: E): string {
 }
 
 =======
+<<<<<<< HEAD
 enum HttpMethod {
   Get,
   Post,
@@ -48,5 +49,31 @@ function getStringValue(e: E): string {
   return String(e);
 }
 
+=======
+enum HttpMethod {
+  Get,
+  Post,
+}
+
+type method = keyof typeof HttpMethod;
+
+// const method: typeof HttpMethod = {
+//   Get: 0,
+//   Post: 1,
+// };
+
+
+
+enum E {
+  A = 10 * 10, // Numeric literal enum member
+  B = 'foo', // String literal enum member
+  C = Math.random(), // Opaque computed enum member
+}
+
+function getStringValue(e: E): string {
+  return String(e);
+}
+
+>>>>>>> f850d2c653fd20aff95d675a367d845c4c964ca1
 >>>>>>> b1e8f3ab43816b47df0b07cfabe48b6b3458468c
 const val = getStringValue(E.A); // "100"
